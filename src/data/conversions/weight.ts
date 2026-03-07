25a013160f13601978a547e2b009d71ac5ce2d91
whimsypingu <blindpingu@gmail.com>
2026-03-07 10:33:11 -0800

    changed daniels function from sigmoid to arcsinh function, which now goes over 1 daniels, but scales exponentially

--- src/data/conversions/weight.ts
+++ src/data/conversions/weight.ts
@@ -10,10 +10,20 @@ export const weight: CategoryData = {
         {
             label: "Metric",
             units: {
+                gram: {
+                    label: 'Grams',
+                    toBase: 'x * 0.001',
+                    fromBase: 'x * 1000'
+                },
                 kilogram: { 
                     label: 'Kilograms', 
                     toBase: 'x * 1',
                     fromBase: 'x * 1'
+                },
+                tonne: {
+                    label: 'Tonnes',
+                    toBase: 'x * 1000',
+                    fromBase: 'x / 1000',
                 },
             }
         },
@@ -35,6 +45,11 @@ export const weight: CategoryData = {
         {
             label: 'Informal Units',
             units: {
+                slug: {
+                    label: 'Slugs',
+                    toBase: 'x * 14.5939',
+                    fromBase: 'x / 14.5939'
+                },
                 kilogrammeSteel: {
                     label: 'Kilogrammes of Steel',
                     toBase: 'x * 1',
@@ -57,8 +72,10 @@ export const weight: CategoryData = {
             units: {
                 daniel: {
                     label: 'Daniels',
-                    toBase: '1 / -0.00001 * (2.71 @ ((0.99 - x) / (x + 0.99)))',
-                    fromBase: '(1.98 / (1 + (2.71 ^ (x * -0.00001)))) - 0.99'
+                    toBase: '((2.71 ^ (x * 10)) - (2.71 ^ (-1 * x * 10))) * 0.5',
+                    fromBase: '2.71 @ (x + ((x ^ 0.5) + 1)) / 10'
+                    //toBase: '1 / -0.00001 * (2.71 @ ((0.99 - x) / (x + 0.99)))',
+                    //fromBase: '(1.98 / (1 + (2.71 ^ (x * -0.00001)))) - 0.99'
                 }
             }
         }
