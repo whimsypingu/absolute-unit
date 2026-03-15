4d02a669dc314a4c091d77db45fd829a790759c3
whimsypingu <blindpingu@gmail.com>
2026-03-15 03:46:22 -0700

    service worker created

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('./sw.js')
+			.then((reg) => console.log('Service Worker registered:', reg.scope))
+			.catch((err) => console.log('Service Worker registration failed:', err));
+	});
+}
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<App />
+	</StrictMode>,
 )
